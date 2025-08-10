51e6c6c8aebad13a3bd7bc4c1345f054e9d9294b
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-08-10 21:05:33 +0900

    [Feat] 레이어 및 Enum타입 추가

--- Assets/Scripts/EnumGroup.cs
+++ Assets/Scripts/EnumGroup.cs
@@ -1,4 +1,25 @@
+using System;
+
 public enum StateType
 {
     Pause, Play, Jump, AttackRed, AttackGreen, AttackBlue
 }
+
+public enum ColorType
+{
+    Red, Green, Blue
+}
+
+public enum HitJudgeMode
+{
+    FirstHit, ComboHIt
+}
+
+public enum LayerType
+{
+    Player = 6, 
+    Note = 7,
+    PlayerAttack = 8,
+    Ground = 9,
+}
+
